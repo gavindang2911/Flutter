52e99a23b22001d0f96d838397a581df9ac03941
gavindang2911 <hoaidang2999@gmail.com>
2020-11-02 02:17:21 -0800

    Adding geology lib

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
+import 'package:clima/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +10,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPosition();
+  }
+
+  void getPosition() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
     );
   }
 }
